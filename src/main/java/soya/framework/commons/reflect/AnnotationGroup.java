5caed07045f11d17c0f5e8f4ba65431928249b96
Qun <wenqun.soya@gmail.com>
2019-10-03 07:23:42 -0700

    DDD support

--- src/main/java/soya/framework/commons/reflect/AnnotationGroup.java
+++ src/main/java/soya/framework/commons/reflect/AnnotationGroup.java
@@ -1,25 +1,47 @@
 package soya.framework.commons.reflect;
 
-import com.google.common.collect.ImmutableMap;
-
-import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
-public final class AnnotationGroup {
+public final class AnnotationGroup extends AnnotatableDescriptor {
+    private static Map<String, AnnotationGroup> groups = new ConcurrentHashMap<>();
+
     private final String name;
-    private final ImmutableMap<String, Object> annotations;
 
     private AnnotationGroup(String name, Map<String, Object> annotations) {
         this.name = name;
-        this.annotations = ImmutableMap.copyOf(annotations);
+        setAnnotations(annotations);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public static String[] groups() {
+        return groups.keySet().toArray(new String[groups.size()]);
+    }
+
+    public static AnnotationGroup get(String name) {
+        return groups.get(name);
+    }
+
+    public static AnnotationGroupBuilder builder(String name) {
+        return new AnnotationGroupBuilder(name);
     }
 
     public static class AnnotationGroupBuilder extends AnnotatableDescriptorBuilder<AnnotationGroupBuilder> {
 
-        private Map<String, Object> annotations = new HashMap<>();
+        private String name;
 
-        private AnnotationGroupBuilder() {
+        private AnnotationGroupBuilder(String name) {
+            this.name = name;
+        }
 
+        public AnnotationGroup build() {
+            if (groups.containsKey(name)) {
+                throw new IllegalStateException("Annotation group '" + name + "' already exists.");
+            }
+            return new AnnotationGroup(name, annotations);
         }
     }
 
--- src/main/java/soya/framework/commons/reflect/DynaEntityTypeDescriptor.java
+++ src/main/java/soya/framework/commons/reflect/DynaEntityTypeDescriptor.java
@@ -2,6 +2,8 @@ package soya.framework.commons.reflect;
 
 import com.google.common.collect.ImmutableMap;
 
+import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class DynaEntityTypeDescriptor extends AnnotatableDescriptor {
@@ -9,8 +11,35 @@ public class DynaEntityTypeDescriptor extends AnnotatableDescriptor {
     private final String name;
     private final ImmutableMap<String, DynaPropertyDescriptor> properties;
 
-    public DynaEntityTypeDescriptor(String name, Map<String, DynaPropertyDescriptor> properties) {
+    private DynaEntityTypeDescriptor(String name, Map<String, DynaPropertyDescriptor> properties, Map<String, Object> annotations) {
         this.name = name;
         this.properties = ImmutableMap.copyOf(properties);
     }
+
+    public static class DynaEntityTypeDescriptorBuilder extends AnnotatableDescriptorBuilder<DynaEntityTypeDescriptorBuilder> {
+        private String name;
+        private Map<String, DynaPropertyDescriptor.DynaPropertyDescriptorBuilder> propertyBuilders = new HashMap<>();
+
+        private DynaEntityTypeDescriptorBuilder() {
+        }
+
+        public DynaEntityTypeDescriptorBuilder name(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public DynaEntityTypeDescriptorBuilder addProperty(String name, DynaPropertyTemplate template) {
+            propertyBuilders.put(name, DynaPropertyDescriptor.builder(name, template));
+            return this;
+        }
+
+        public DynaEntityTypeDescriptor build() {
+            Map<String, DynaPropertyDescriptor> properties = new LinkedHashMap<>();
+            propertyBuilders.entrySet().forEach(e -> {
+                properties.put(e.getKey(), e.getValue().build());
+            });
+            return new DynaEntityTypeDescriptor(name, properties, annotations);
+        }
+    }
+
 }
